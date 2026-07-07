ed102808d68e77ec838dce59ecdd386256ef9f65
ruby23calvi <ruby23calvi@outlook.com>
2026-07-07 17:07:24 +0530

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+import "./Header.css";
+
+
+function Header(){
+
+return(
+
+<header className="header">
+
+<h1>
+Acceptance Insurance
+</h1>
+
+
+<p>
+Smart insurance expense management platform
+</p>
+
+
+</header>
+
+);
+
+}
+
+
+export default Header;
